d99aa079a2f91dd595fa497eb8404a74c3ac1138
aichingert <aichinger.tobias13@gmail.com>
2023-10-19 10:51:59 +0200

    feat: db

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR="./db"
+
+if test ! -d $DIR; then
+	mkdir $DIR
+	chmod -R o+w $DIR
+fi
+
+docker run -d --name oradb \
+-p 1521:1521 \
+-e ORACLE_PWD=lol \
+-e ORACLE_CHARACTERSET=AL32UTF8 \
+-v $DIR:/opt/oracle/oradata \
+container-registry.oracle.com/database/free:latest
