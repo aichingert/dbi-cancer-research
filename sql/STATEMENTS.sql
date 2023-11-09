e0da0d3546dae92a5da0549eddc830a1aa1715d9
Nikolaus-Alexander <alexandernikolaus04@gmail.com>
2023-11-09 11:39:49 +0100

    STATEMENTS + changes

---
+++ sql/STATEMENTS.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PACKAGE CANCER_Research AS
+    PROCEDURE get_connected_genes(gene NUMBER);
+END CANCER_Research;
+/
+
+CREATE OR REPLACE PACKAGE BODY CANCER_Research AS
+
+    PROCEDURE get_connected_genes(geneID NUMBER) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------------------');
+        FOR gen IN (SELECT gene2_id
+                         FROM MAPPING
+                         WHERE gene1_id = geneID)
+            LOOP
+                DBMS_OUTPUT.PUT_LINE('Connected Gene ID: ' || gen.GENE2_ID);
+            END LOOP;
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------------------');
+
+    END get_connected_genes;
+END CANCER_Research;
+/
+BEGIN
+CANCER_Research.get_connected_genes(1);
+END;
+/
--- sql/insert.sql
+++ sql/insert.sql
@@ -4,10 +4,21 @@ INSERT INTO BEING (being_id, name) VALUES (2, 'Mouse');
 INSERT INTO BEING (being_id, name) VALUES (3, 'Jade');
 
 --GENE
-INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (2, 1, 'Gene2_Human', 0.6);
-INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (3, 2, 'Gene1_Mouse', 0.7);
-INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (4, 2, 'Gene2_Mouse', 0.5);
-INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (5, 3, 'Gene1_Jada', 0.4);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (1, 1, 'GeneX', 0.6);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (2, 1, 'GeneD', 0.6);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (3, 2, 'GeneS', 0.7);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (4, 2, 'GeneAS', 0.5);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (5, 3, 'GeneGA', 0.4);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (6, 3, 'GeneH', 0.4);
+INSERT INTO GENE (gene_id, being_id, name, essential_score) VALUES (7, 3, 'GeneJ', 0.2);
+
+
+INSERT INTO MAPPING (gene1_id, gene2_id) VALUES (1,3);
+INSERT INTO MAPPING (gene1_id, gene2_id) VALUES (1,6);
+INSERT INTO MAPPING (gene1_id, gene2_id) VALUES (1,7);
+INSERT INTO MAPPING (gene1_id, gene2_id) VALUES (2,5);
+INSERT INTO MAPPING (gene1_id, gene2_id) VALUES (2,4);
+
 
 SELECT * FROM GENE;
 
