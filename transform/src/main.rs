96ad98b20ba6a8ae95ae634b466aaafe690564e3
aichingert <aichinger.tobias13@gmail.com>
2023-11-18 11:47:26 +0100

    feat: able to get gene and soon lethality of human gene

--- transform/src/main.rs
+++ transform/src/main.rs
@@ -1,72 +1,47 @@
-use oracle::{Connection, Result};
 use axum::{
     extract::{Path, State}, 
     routing::get, 
-    Router
+    Router,
+    Json
 };
 
 mod models;
 use models::{AppState, Gene};
 
-const LETHALITY_SQL: &'static str = "SELECT * FROM SynLeth WHERE geneId1 = :1 OR geneId2 = :1";
+const LETHALITY_SQL: &'static str = "SELECT * FROM SYN_LETH WHERE GENE1_ID = :1 OR GENE2_ID = :1";
 const ESSENTIALITY_SQL: &'static str ="SELECT * FROM Gene WHERE name = :1 AND essentialScore <> NULL";
 
 async fn get_lethality(
-    Path(gene): Path<String>,
+    Path(ident): Path<String>,
     State(state): State<AppState>,
 ) -> Json<Vec<String>> {
     let connection = match state.get_connection() {
         Ok(conn) => conn,
-        Err(e) => {
+        Err(_) => {
             dbg!("ERROR: failed to connect to DB");
-            return Json(vec![]);
+            return Json(vec!["Internal Server Error".to_string()]);
         }
     };
 
-    // TODO: check errors
-    let lethal_rows   = connection.query(LETHALITY_SQL, &[&gene]).unwrap();
-    let essential_row = connection.query(ESSENTIALITY_SQL, &[&gene]).unwrap();
+    // Implement into Response for result and propagate errors with ?
+    let gene = Gene::new(&ident.to_uppercase(), &connection);
 
-    println!("{:?}", lethal_rows);
+    if gene.is_err() {
+        return Json(vec!["no value".to_string()]);
+    };
 
-    for lethal_row in lethal_rows {
-        let row = lethal_row.unwrap();
-
-        println!("{:?}", row);
-    }
+    println!("{:?}", gene);
 
     Json(Vec::new())
-}
-
-fn test_query(connection: &Connection) -> Result<()> {
-    let sql = "select * from being";
-    let mut stmt = connection.statement(sql).build()?;
-    let rows = stmt.query(&[])?;
-
-rows    for row_result in rows {
-        // print column values
-        for (idx, val) in row_result?.sql_values().iter().enumerate() {
-            if idx != 0 {
-                print!(",");
-            }
-            print!("{}", val);
-        }
-        println!();
-    }
-
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     let state = AppState::new("system", "lol", "localhost:1521");
-    let connection = state.get_connection();
 
     let app: Router = Router::new()
         .route("/:gene", get(get_lethality))
         .with_state(state);
-
-    test_query(&connection.unwrap()).unwrap();
 
     dbg!("Listening on localhost:3000");
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
--- transform/src/models.rs
+++ transform/src/models.rs
@@ -2,6 +2,9 @@ use std::sync::Arc;
 use oracle::Connection;
 use serde::Deserialize;
 
+const GENE_SQL: &'static str = "SELECT gene_id, being_id, name, essential_score FROM gene WHERE name = :1";
+const GENE_LETHALITY: &'static str = "SELECT GENE1_ID, GENE2_ID, SCORE FROM SYN_LETH WHERE GENE1_ID = :1 OR GENE2_ID = :1";
+
 #[derive(Clone)]
 pub struct AppState {
     username: Arc<str>,
@@ -25,5 +28,29 @@ impl AppState {
 
 #[derive(Debug, Deserialize)]
 pub struct Gene {
+    pub id: i64,
     pub name: String,
+    pub being: String,
+    pub score: Option<f32>,
+}
+
+impl Gene {
+    pub fn new(ident: &str, connection: &Connection) -> Result<Self, oracle::Error> {
+        let row = connection.query_row(GENE_SQL, &[&ident])?;
+
+        Ok(Self {
+            id: row.get("gene_id").unwrap(),
+            name: row.get("name").unwrap(),
+            being: row.get("being_id").unwrap(),
+            score: row.get("essential_score").unwrap(),
+        })
+    }
+
+    pub fn is_lethal(&self, connection: &Connection) -> Result<String, oracle::Error> {
+        let results = connection.query(GENE_LETHALITY, &[&self.id])?;
+
+
+
+        Ok(String::new())
+    }
 }
